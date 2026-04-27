d8a9da707371a0e5c5dd4312b08b4e0b9a7dcfa3
Sumeet <sadhanagupta0324@gmail.com>
2026-04-27 23:46:48 +0530

    Doc: add swagger

---
+++ backend/src/config/swagger.ts
@@ -0,0 +1,20 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Cricnerd Backend REST API",
+      version: "1.0.0",
+      description: "API documentation for Cricket Tournament System",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"], 
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
