491c1223a8b7eb5c3cd74d7f3d9cf1ae0ab74c29
Sumeet <sadhanagupta0324@gmail.com>
2026-06-01 21:17:10 +0530

    Add: Tournament model and ZOD schema

---
+++ backend/src/controllers/tournament/tournament.schema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { PLAYING_FORMAT } from "../../constants/playingFormat.constant";
+import { PLAYING_STATUS } from "../../constants/playingStatus.constant";
+
+// startDate: z.iso.date()
+// Logic error previosly used - 
+// startDate: z.coerce.date().min(new Date(),{message: "You can't start a tournament in past"}),
+// endDate: z.coerce.date().min(new Date(),{message: "You can't end a tournament in past"}),
+export const tournamentSchema = z.object({
+    name:z.string(),    
+    startDate: z.coerce.date(),
+    endDate: z.coerce.date(),
+    playingFormat: z.enum(PLAYING_FORMAT),
+    playingStatus: z.enum(PLAYING_STATUS).default(PLAYING_STATUS[0]).optional()
+})
+
+export const updateTournamentStatusSchema = z.object({
+  playingStatus: z.enum(PLAYING_STATUS),
+});
---
+++ backend/src/models/tournament.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Mongoose } from "mongoose";
+
+import { PLAYING_FORMAT } from "../constants/playingFormat.constant"
+import { PLAYING_STATUS } from "../constants/playingStatus.constant";
+
+const tournamentSchema = new mongoose.Schema({
+    organizerId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true, 
+    },
+    tournamentName:{
+        type: String,
+        required: true
+    },
+   startDate:{
+        type: Date,
+        required: true,
+        min: Date.now()
+   },
+   endDate:{
+        type: Date,
+        required: true,
+        min: Date.now()
+   },
+   playingFormat:{
+        type: String,
+        enum: PLAYING_FORMAT,
+        required: true
+   },
+   playingStatus:{
+    type: String,
+    enum: PLAYING_STATUS,
+    default: PLAYING_STATUS[0]
+   }
+})
+
+export const Tournament = mongoose.model("tournament", tournamentSchema);
