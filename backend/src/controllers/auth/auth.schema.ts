e8c1df40027aeb582ba65a9c69e3cec0348dbcd9
Sumeet <sadhanagupta0324@gmail.com>
2026-06-03 01:03:44 +0530

    feat: password validation via ZOD

--- backend/src/controllers/auth/auth.schema.ts
+++ backend/src/controllers/auth/auth.schema.ts
@@ -5,15 +5,20 @@ export const registerSchema = z.object({
   email: z.string().email("Invalid email address").toLowerCase().trim(),
   password: z
     .string()
-    .min(6, { message: "Password must be at least 6 characters" }),
+    .min(8, "Password must be at least 8 characters")
+    .regex(/[A-Z]/, "Must contain uppercase letter")
+    .regex(/[a-z]/, "Must contain lowercase letter")
+    .regex(/[0-9]/, "Must contain a number")
+    .regex(/[^A-Za-z0-9]/, "Must contain special character")
+    .trim(),
   username: z
     .string()
     .min(2, { message: "Username must be at least 2 characters" })
     .trim(),
   role: z
     .array(z.enum(ROLE))
+    .optional()
     .default([ROLE[0]])
-
 });
 
 export const loginSchema = z.object({
