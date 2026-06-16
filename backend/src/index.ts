76018e2fc8680cca2cb5b8fb75ee9acc9ba3171d
Sumeet <sadhanagupta0324@gmail.com>
2026-06-16 23:29:20 +0530

    Add: Socket init function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import { connectDB } from "./db/db";
 import app from "./app"
 import { createServer } from "node:http";
-
+import { initSocket } from "./services/socket.service";
 
 
 async function startDB(){
     await connectDB().then(() => console.log("DB connected"))
 
     const server = createServer(app)
+    initSocket(server);
+    
     const PORT = process.env.PORT || 3000
     server.listen(PORT,()=>{
         console.log("Server Connected");
